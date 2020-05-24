090df45a614bb6224ec8a2687c220d28acf44c29
Loup <loup.peluso@gmail.com>
2020-05-25 00:14:36 +0200

    fix: removed concurrently

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,4 @@
-rm -rf node_modules client/build client/node_modules client/build server/node_modules server/dist && \
-yarn && \
-concurrently \
-    "(cd server && yarn && yarn lint && CI=true yarn test && yarn build)" \
-    "(cd client && yarn && CI=true yarn test && yarn build)"
+rm -rf node_modules client/build client/node_modules client/build server/node_modules server/dist
+yarn
+(cd server && yarn && yarn lint && CI=true yarn test && yarn build)
+(cd client && yarn && CI=true yarn test && yarn build)
