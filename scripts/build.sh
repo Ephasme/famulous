ea9f1255599aafd7d969e3253eecba7efb74c9bb
Loup <loup.peluso@gmail.com>
2020-05-27 00:49:27 +0200

    fix: test

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 DIR=$(dirname $0)
 ROOT=$DIR/..
 $DIR/setup.sh
-(cd server && yarn build)
-(cd client && yarn build)
+(yarn --cwd $ROOT/server build)
+(yarn --cwd $ROOT/client build)
 cp -r $ROOT/client/build $ROOT/server/dist/client
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,5 +7,7 @@ rm -rf node_modules \
     $ROOT/server/node_modules \
     $ROOT/server/dist
 yarn
+echo "installing server dependencies"
 (cd $ROOT/server && yarn)
+echo "installing client dependencies"
 (cd $ROOT/client && yarn)
