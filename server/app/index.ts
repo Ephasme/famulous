4da687423c253408dd5a1a0c5f0a82392debac77
Loup <loup.peluso@gmail.com>
2020-05-30 23:35:09 +0200

    fix: async setup for db

--- server/app/index.ts
+++ server/app/index.ts
@@ -2,20 +2,19 @@ import * as express from "express";
 import * as path from "path";
 
 import userRoutes from "./users/routes";
-import db from "../infra/db/db";
+import setupDb from "../infra/db/db";
 
 const app = express();
 const port = parseInt(process.env.PORT || "3001");
 
-app.get("/api/give-me-something", (req, res) => {
-  res.send({ value: "something" });
+setupDb().then((db) => {
+  if (process.env.NODE_ENV === "production") {
+    console.log("Application started in production mode.");
+    app.use(express.static(path.join(__dirname, "../../client/build")));
+  }
+  app.get("/api/give-me-something", (req, res) => {
+    res.send({ value: "something" });
+  });
+  app.use("/users", userRoutes(db));
+  app.listen(port, () => console.log(`App started on port ${port}`));
 });
-
-if (process.env.NODE_ENV === "production") {
-  console.log("Application started in production mode.");
-  app.use(express.static(path.join(__dirname, "../../client/build")));
-}
-
-app.use("/users", userRoutes(db));
-
-app.listen(port, () => console.log(`App started on port ${port}`));
--- server/infra/db/db.ts
+++ server/infra/db/db.ts
@@ -1,20 +1,28 @@
 import * as Knex from "knex";
 
-const { DATABASE_URL } = process.env
-if (!DATABASE_URL) {
-  throw new Error("Missing environment variable: DATABASE_URL");
+const instance: Knex | null = null;
+
+export default async function (): Promise<Knex> {
+  if (instance) return instance;
+
+  const { DATABASE_URL } = process.env;
+  if (!DATABASE_URL) {
+    throw new Error("Missing environment variable: DATABASE_URL");
+  }
+
+  console.log(`creating db connection to ${DATABASE_URL}`)
+  const knex = Knex({
+    client: "postgres",
+    connection: DATABASE_URL,
+    migrations: {
+      database: "knex_migrations",
+      directory: "./server/infra/db/migrations",
+    },
+    pool: { min: 0, max: 4 },
+  });
+
+  console.log("migrating databases")
+  await knex.migrate.latest();
+
+  return knex
 }
-
-const knex = Knex({
-  client: "postgres",
-  connection: DATABASE_URL,
-  migrations: {
-    database: "knex_migrations",
-    directory: "./server/infra/db/migrations"
-  },
-  pool: { min: 0, max: 4 },
-});
-
-knex.migrate.latest();
-
-export default knex;
