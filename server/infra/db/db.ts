b6046c40e7379f1f18b01400b9f5e065e864d6ac
Loup <loup.peluso@gmail.com>
2020-05-31 00:16:28 +0200

    fix: try to change path when production

--- server/infra/db/db.ts
+++ server/infra/db/db.ts
@@ -9,20 +9,25 @@ export default async function (): Promise<Knex> {
   if (!DATABASE_URL) {
     throw new Error("Missing environment variable: DATABASE_URL");
   }
-
-  console.log(`creating db connection to ${DATABASE_URL}`)
+  const migrations = process.env.NODE_ENV === "production"
+    ? "./dist/infra/db/migrations"
+    : "./server/infra/db/migrations"
+       
+  console.log(`creating db connection to ${DATABASE_URL}`);
   const knex = Knex({
     client: "postgres",
     connection: DATABASE_URL,
     migrations: {
       database: "knex_migrations",
-      directory: "./server/infra/db/migrations",
+      directory: migrations,
     },
     pool: { min: 0, max: 4 },
   });
 
-  console.log("migrating databases")
-  await knex.migrate.latest();
+  console.log("migrating databases");
+  await knex.migrate.latest({
+    
+  });
 
-  return knex
+  return knex;
 }
