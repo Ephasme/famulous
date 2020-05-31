5b8da38125a9934b106f078e6f3f5c18d832ab50
Loup <loup.peluso@gmail.com>
2020-05-31 15:21:24 +0200

    fix: try improve release phase

---
+++ knexfile.ts
@@ -0,0 +1,17 @@
+import * as dot from "dotenv";
+dot.config();
+
+const { DATABASE_URL } = process.env;
+const config = {
+  client: "postgresql",
+  connection: DATABASE_URL,
+  migrations: {
+    database: "knex_migrations",
+    directory: "./server/infra/db/migrations",
+  },
+};
+
+module.exports = {
+  development: config,
+  production: config,
+};
--- server/infra/db/db.ts
+++ server/infra/db/db.ts
@@ -1,32 +1,19 @@
 import * as Knex from "knex";
 
 const instance: Knex | null = null;
+const { DATABASE_URL } = process.env;
+if (!DATABASE_URL) {
+  throw new Error("Missing environment variable: DATABASE_URL");
+}
 
 export default async function (): Promise<Knex> {
   if (instance) return instance;
 
-  const { DATABASE_URL } = process.env;
-  if (!DATABASE_URL) {
-    throw new Error("Missing environment variable: DATABASE_URL");
-  }
-  const migrations = process.env.NODE_ENV === "production"
-    ? "./dist/infra/db/migrations"
-    : "./server/infra/db/migrations"
-       
   console.log(`creating db connection to ${DATABASE_URL}`);
   const knex = Knex({
     client: "postgres",
     connection: DATABASE_URL,
-    migrations: {
-      database: "knex_migrations",
-      directory: migrations,
-    },
     pool: { min: 0, max: 4 },
-  });
-
-  console.log("migrating databases");
-  await knex.migrate.latest({
-    
   });
 
   return knex;
