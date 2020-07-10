f4d11d0d65850e78a18780ba5256db2dc3011639
David Clochard <david.clochard@foxintelligence.fr>
2020-07-10 16:03:52 +0200

    Login page first version

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,24 +2,38 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import { LoginPage } from "./LoginPage";
-import Routes from "./Routes";
+import Routes from "./routes/Routes";
+import { SuccesfulLogin } from "./routes/LoginPage";
 
 type Props = { whatever: string };
-type State = { connected: boolean };
+type State = { userEmail: string | null };
 
 class App extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { connected: false };
+    this.state = { userEmail: localStorage.getItem("email") };
   }
+
+  onUserConnection = ({ email, token }: SuccesfulLogin) => {
+    console.log({ email, token });
+
+    localStorage.setItem("email", email);
+    localStorage.setItem("token", token);
+
+    this.setState({ userEmail: email });
+
+    return null;
+  };
+
   render() {
     return (
       <Router>
         <div className="App">
           <header className="App-header">
-            <Routes />
-            {/* <LoginPage onSubmit={({ email, password }) => null} /> */}
+            <Routes
+              onUserConnection={this.onUserConnection}
+              userIsConnected={!!this.state.userEmail}
+            />
           </header>
         </div>
       </Router>
